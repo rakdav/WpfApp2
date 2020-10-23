dfe22be75e0678b3957d49b92839dc8b17fb12da
Gogs <gogs@fake.local>
2020-10-23 17:25:13 +0200

    Добавьте файлы проекта.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int n = int.Parse(input.Text);
+                int S = 0;
+                int i = 1;
+                while(i<=n)
+                {
+                    S += i;
+                    i++;
+                }
+                output.Text = S.ToString();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int n = int.Parse(input.Text);
+                int S = 0;
+                int i = 1;
+                do
+                {
+                    S += i;
+                    i++;
+                }
+                while (i<=n);
+                output.Text = S.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int n = int.Parse(input.Text);
+                int S = 0;
+                for(int i=1;i<=n;i++)
+                {
+                    S += i;
+                }
+                output.Text = S.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
